ff121aa16b24a0056da2c4dffe037aed355d7f95
Administrator <Administrator@A316-TH>
2026-03-10 14:01:40 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Class_Ch12_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
